434b32d9921205c02a9e147eff73820c95e7d746
toufiq-austcse <sadydic@gmail.com>
2019-08-15 15:43:39 -0700

    Initial Commit

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ let usersRouter = require('./routes/users');
 let activityRecordsRouter = require('./routes/activityRecords');
 
 let app = express();
+//For handling CORS Error
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
